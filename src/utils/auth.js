7e937137211cc861ba3aeefbebc15f642bb896c4
Menatalla Gomaa <minafathi98@gmail.com>
2024-06-20 22:06:20 +0200

    auth.js created

---
+++ src/utils/auth.js
@@ -0,0 +1,38 @@
+import auth0 from 'auth0-js'
+
+const webAuth = new auth0.WebAuth({
+  domain: 'ishan-me.auth0.com',
+  clientID: 'Dvi0nHAOAA8wJgITRzmKPeu5Iv3emk02',
+  redirectUri: 'http://localhost:8080/callback',
+  responseType: 'token id_token',
+  scope: 'openid profile'
+})
+
+let token = {}
+
+const handleAuth = (cb) => {
+  webAuth.parseHash((error, authResult) => {
+    if (authResult && authResult.accessToken && authResult.idToken) {
+      token.accessToken = authResult.accessToken
+      token.idToken = authResult.idToken
+      token.expiry = new Date().getTime() + authResult.expiresIn * 1000
+      cb()
+    } else {
+      console.log(error)
+    }
+  })
+}
+
+const isLogged = () => {
+  return token.accessToken && new Date().getTime() < token.expiry
+}
+
+const login = () => {
+  webAuth.authorize()
+}
+
+const logUserOut = () => {
+  token = {}
+}
+
+export { login, handleAuth, isLogged, logUserOut }
